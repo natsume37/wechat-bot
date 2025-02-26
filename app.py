8a3e39bbbd1ddae8e88163c34349c4b62923bcff
natsume37 <natsume37@outlook.com>
2025-02-26 17:00:05 +0800

    整合

---
+++ app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Project ：wechat_oa
+@File    ：app
+@IDE     ：PyCharm
+@Author  ：Martin
+@Date    ：2025/2/26 16:52
+@Desc    ：文件描述
+"""
+
---
+++ core/core.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Project ：wechat_oa
+@File    ：core
+@IDE     ：PyCharm
+@Author  ：Martin
+@Date    ：2025/2/26 16:53
+@Desc    ：文件描述
+"""
+
